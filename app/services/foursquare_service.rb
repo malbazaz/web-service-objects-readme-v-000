df8df7a6c4dd0de4c7ca6f6089dbaa10900408c9
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-03 14:33:27 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,5 +1,5 @@
 class FourSquareService
-  def create
+  def authenticate!
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
       req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
